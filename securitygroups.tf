579b3c82048f5db347403da9c2ddc160225d5efe
sathyarajdurai <sathyaraj.durai@cloudreach.com>
2022-08-07 19:39:16 +0530

    second commit

--- securitygroups.tf
+++ securitygroups.tf
@@ -11,13 +11,13 @@ resource "aws_security_group" "blue_green_server" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
-  ingress {
-    description      = "Allow port 22"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+  # ingress {
+  #   description      = "Allow port 22"
+  #   from_port        = 22
+  #   to_port          = 22
+  #   protocol         = "tcp"
+  #   cidr_blocks      = ["0.0.0.0/0"]
+  # }
 
   egress {
     from_port        = 0
